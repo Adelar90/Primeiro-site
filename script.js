5efd5e024cf3c04f4efdb68f41249f2d8c221691
Adelar <adelar.ferreira@escola.pr.gov.br>
2026-06-18 09:01:42 -0300

    contraste

--- script.js
+++ script.js
@@ -1,7 +1,31 @@
- const botao = document.querySelector("button")
-    botao.addEventListener('click', curtir)
+const botao = document.querySelector("button")
+botao.addEventListener('click', curtir)
 
-    function curtir(){
-        let curtidas = document.querySelector("span")
-        curtidas.textContent++;
-    }
+function curtir() {
+    let curtidas = document.querySelector("span")
+    curtidas.textContent++;
+}
+
+
+// BOTÃO DO TAMANHO
+const tamanho = document.getElementById("tamanho")
+tamanho.addEventListener("click", aumentarFonte)
+let fonte = 16;
+
+//FUNÇÃO
+function aumentarFonte(){
+    fonte++;
+    document.body.style.fontSize = fonte + "px";
+}
+
+//BOTÃO DO CONTRASTE
+const contraste = document.getElementById("contraste")
+contraste.addEventListener("click", alterarCor )
+
+
+ // FUNÇÃO DO CONTRASTE
+ function alterarCor(){
+
+    document.body.style.color= "yellow";
+}
+
